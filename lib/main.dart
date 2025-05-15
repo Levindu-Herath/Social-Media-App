423b21e610603fbaecee8f9beb1e07cf50f63812
Levindu Herath <levinduherath@gmail.com>
2025-05-15 20:38:44 +0530

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media/firebase_options.dart';
 
-void main() {
+void main() async {
+  // firebase setup
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
