5aaaabe2335de149ed88a35c3c5cc42108863d74
Levindu Herath <levinduherath@gmail.com>
2025-05-15 20:04:13 +0530

    Auth repo Created

---
+++ lib/features/auth/domain/entities/repos/auth_repo.dart
@@ -0,0 +1,19 @@
+/*
+  Auth Repository - outlines the  possible auth operations for this app
+                    (not actually implemented here)
+*/
+
+import 'package:social_media/features/auth/domain/entities/app_user.dart';
+
+abstract class AuthRepo {
+  Future<AppUser?> loginWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+  Future<AppUser?> registerWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<AppUser?>getCurrentUser();
+}
